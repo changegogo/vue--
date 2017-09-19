61a842c07cfab2fe2715c9bdde016aa9a56cc8e9
dailiwang <758017188@qq.com>
2017-09-19 09:20:10 +0800

    music

---
+++ build/server.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var express = require('express');
+var axios = require('axios');
+var port = 3000;
+var app = express();
+
+var apiRoutes = express.Router();
+
+apiRoutes.get('/getDiscList',(req,res)=>{
+	var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+	axios.get(url,{
+		headers:{
+			referer: 'https://c.y.qq.com/',
+			host: 'c.y.qq.com'
+		},
+		params: req.query
+	}).then((response)=>{
+		res.json(response.data) 
+	}).catch((e)=>{
+		console.log(e)
+	})
+});
+
+apiRoutes.get('/getLyric',(req,res)=>{
+	var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+	axios.get(url,{
+		headers:{
+			referer: 'https://c.y.qq.com/',
+			host: 'c.y.qq.com'
+		},
+		params: req.query
+	}).then((response)=>{
+		var ret = response.data
+		if(typeof ret === 'string'){
+			var reg = /^\w+\(({[^()]+})\)$/
+			var matches = ret.match(reg)
+			if(matches){
+				ret = JSON.parse(matches[1])
+			}
+		}
+		res.json(ret) 
+	}).catch((e)=>{
+		console.log(e)
+	})
+});
+
+app.use('/api',apiRoutes);
+
+app.use(express.static('dist'));
+
+
+console.log('> Starting dev server 3000...');
+
+var server = app.listen(port);
